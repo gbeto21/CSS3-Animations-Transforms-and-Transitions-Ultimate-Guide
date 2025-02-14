b7bae2f2503ba1385938ee1db38ff48d11167778
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-02-14 15:04:58 -0600

    Mouse position aware circle fill hover effect

---
+++ Section6_CoolButtonEffects/Mouse position aware circle fill hover effect/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+  $('.button.first').on('mouseenter', function (event) {
+
+    $(this).find('.btn-bg').css({ top: event.offsetY, left: event.offsetX });
+
+  });
+
+});
